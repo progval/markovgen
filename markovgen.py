e3f0117ace42686bb13695d9a5e66b31f33dd19b
Valentin Lorentz <progval@progval.net>
2014-10-20 20:00:39 +0200

    Add support for line breaks and weechat logs format.

--- markovgen.py
+++ markovgen.py
@@ -1,52 +1,64 @@
+import re
+import sys
 import random
 
 class Markov(object):
 
-    def __init__(self, open_file):
+    def __init__(self, messages):
         self.cache = {}
-        self.open_file = open_file
-        self.words = self.file_to_words()
-        self.word_size = len(self.words)
-        self.database()
+        self.words = ['\n']
+        for message in messages:
+            self.feed(message)
 
 
-    def file_to_words(self):
-        self.open_file.seek(0)
-        data = self.open_file.read()
-        words = data.split()
-        return words
-
-
-    def triples(self):
+    def triples(self, words):
         """ Generates triples from the given data string. So if our string were
                 "What a lovely day", we'd generate (What, a, lovely) and then
                 (a, lovely, day).
         """
 
-        if len(self.words) < 3:
+        if len(words) < 3:
             return
 
-        for i in range(len(self.words) - 2):
-            yield (self.words[i], self.words[i+1], self.words[i+2])
+        for i in range(len(words) - 2):
+            yield (words[i], words[i+1], words[i+2])
 
-    def database(self):
-        for w1, w2, w3 in self.triples():
+    def feed(self, message):
+        splitted = message.split(' ')
+        for w1, w2, w3 in self.triples(self.words[-1:] + splitted + ['\n']):
             key = (w1, w2)
             if key in self.cache:
                 self.cache[key].append(w3)
             else:
                 self.cache[key] = [w3]
+        self.words.extend(splitted + ['\n'])
 
-    def generate_markov_text(self, size=25):
-        seed = random.randint(0, self.word_size-3)
-        seed_word, next_word = self.words[seed], self.words[seed+1]
-        w1, w2 = seed_word, next_word
+    def generate_markov_text(self, max_size=30):
+        seed_word = '\n'
+        while seed_word == '\n' or next_word == '\n':
+            seed = random.randint(0, len(self.words)-3)
+            seed_word, next_word = self.words[seed], self.words[seed+1]
+        if random.choice([True, False, False]) and ('\n', seed_word) in self.cache:
+            w1, w2 = '\n', seed_word
+        else:
+            w1, w2 = seed_word, next_word
         gen_words = []
-        for i in xrange(size):
+        for i in range(max_size):
             gen_words.append(w1)
-            w1, w2 = w2, random.choice(self.cache[(w1, w2)])
-        gen_words.append(w2)
-        return ' '.join(gen_words)
+            new = '\n'
+            new = random.choice(self.cache[(w1, w2)])
+            if new == '\n':
+                break
+            w1, w2 = w2, new
+        if w2 != '\n':
+            gen_words.append(w2)
+        return ' '.join(filter(lambda x:x!='\n', gen_words))
 
 
+extracter = re.compile('^.*\t.+\t(<[^ ]+> )?(?P<message>.*)$')
+messages = [extracter.match(x) for x in open(sys.argv[1]).readlines()]
+messages = [x.group('message') for x in messages if x]
 
+m = Markov(messages)
+for x in range(0, 500):
+    print(m.generate_markov_text())
