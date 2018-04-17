cb53fbf245f0554581a67fd611f006e67ce0c575
Valentin Lorentz <progval+git@progval.net>
2018-04-17 09:31:44 +0200

    rewrite using sqlite instead of dicts (less RAM, a lot more CPU)

--- markovgen/markovgen.py
+++ markovgen/markovgen.py
@@ -3,6 +3,8 @@ import re
 import sys
 import codecs
 import random
+import sqlite3
+import tempfile
 import logging
 
 __all__ = ['Markov', 'mixed_encoding_extracting', 'REGEXPS']
@@ -23,12 +25,21 @@ if sys.version_info[0] >= 3:
 class Markov(object):
 
     def __init__(self, messages=None):
-        self.forward_cache = {}
-        self.backward_cache = {}
-        self.words = ['\n']
+        self._create_db()
+        self.last_words = []
         if messages:
             for message in messages:
                 self.feed(message)
+
+    def _create_db(self):
+        #self._tempfile = tempfile.NamedTemporaryFile()
+        #self.db = c = sqlite3.connect(self._tempfile.name)
+        self.db = c = sqlite3.connect(':memory:')
+        with c:
+            #c.execute('CREATE TABLE triples (w1, w2, w3, nb, CONSTRAINT uniqueness UNIQUE(w1, w2, w3));')
+            c.execute('CREATE TABLE triples (w1, w2, w3);')
+            #c.execute('CREATE INDEX forward ON triples (w1, w2)')
+            #c.execute('CREATE INDEX backward ON triples (w2, w3)')
 
 
     def triples(self, words):
@@ -43,18 +54,15 @@ class Markov(object):
         for i in range(len(words) - 2):
             yield (words[i], words[i+1], words[i+2])
 
-    def _add_key_to_cache(self, key, cache, w):
-        if key in cache:
-            cache[key].append(w)
-        else:
-            cache[key] = [w]
-
     def feed(self, message):
         splitted = list(map(intern, message.split(' ')))
-        for w1, w2, w3 in self.triples(self.words[-2:] + splitted + ['\n']):
-            self._add_key_to_cache((w1, w2), self.forward_cache, w3)
-            self._add_key_to_cache((w3, w2), self.backward_cache, w1)
-        self.words.extend(splitted + ['\n'])
+        with self.db:
+            #for triple in self.triples(self.last_words[-2:] + splitted + ['\n']):
+            #    self.db.execute('INSERT OR IGNORE INTO triples VALUES (?, ?, ?, 0);', triple)
+            #    self.db.execute('UPDATE triples SET nb=nb+1 WHERE w1==? AND w2==? and w3==?;', triple)
+            self.db.executemany('INSERT INTO triples VALUES (?, ?, ?);',
+                    self.triples(self.last_words[-2:] + splitted + ['\n']))
+        self.last_words = (self.last_words + splitted[0:2])[-2:]
 
     def feed_from_file(self, fd, extracter):
         list(map(self.feed, filter(bool, map(extracter, fd.readlines()))))
@@ -63,26 +71,18 @@ class Markov(object):
         d = -1 if backward else +1
         if not seed_word:
             # Select a random seed and a random next word
-            seed_word = '\n'
-            while seed_word == '\n' or next_word == '\n':
-                seed = random.randint(1, len(self.words)-3)
-                seed_word, next_word = self.words[seed], self.words[seed+d]
+            c = self.db.cursor()
+            c.execute('SELECT w1, w2 FROM triples WHERE w1 != "\\n" AND w2 != "\\n" ORDER BY random() LIMIT 1;')
+            (seed_word, next_word) = c.fetchone()
         elif seed_word in self.words:
             # List the indexes of the occurences of the seed in the words,
             # select one of them, and take the next word.
-            possible_indexes = [i+1 for (i, x) in enumerate(self.words[1:-1])
-                              if self.words[i+1] == seed_word]
-            index = random.choice(possible_indexes)
-            next_word = self.words[index+d]
+            c = self.db.cursor()
+            c.execute('SELECT w1, w2 FROM triples WHERE w1 == ? AND w2 != "\\n" ORDER BY random() LIMIT 1;', (seed_word,))
+            (seed_word, next_word) = c.fetchone()
         else:
             raise ValueError('%s is not in the corpus.' % (seed_word,))
         return (seed_word, next_word)
-
-    def available_seeds(self, backward=False):
-        if backward:
-            return self.backward_cache.keys()
-        else:
-            return self.forward_cache.keys()
 
     def generate_markov_text(self, max_size=30, seed=None, backward=False,
             seed_word=None):
@@ -94,20 +94,34 @@ class Markov(object):
             (seed_word, next_word) = seed
         else:
             (seed_word, next_word) = self.select_seed(seed, backward)
-        cache = self.backward_cache if backward else self.forward_cache
 
-        if random.choice([True, False, False]) and ('\n', seed_word) in cache:
-            w1, w2 = '\n', seed_word
-        else:
-            w1, w2 = seed_word, next_word
+        c = self.db.cursor()
+
+
+        w1, w2 = seed_word, next_word
+        if random.choice([True, False, False]):
+            if backward:
+                c.execute('SELECT w1 FROM triples WHERE w2 == ? AND w3 == "\\n" ORDER BY random() LIMIT 1', (seed_word,))
+            else:
+                c.execute('SELECT w3 FROM triples WHERE w1 == "\\n" AND w2 == ? ORDER BY random() LIMIT 1', (seed_word,))
+            r = c.fetchone()
+            if r is not None:
+                w1, w2 = r
+
         (w1, w2) = intern(w1), intern(w2)
         gen_words = []
         for i in range(max_size):
             gen_words.append(w1)
             new = '\n'
-            if (w1, w2) not in cache:
+            if backward:
+                c.execute('SELECT w1 FROM triples WHERE w2 == ? AND w3 == ? ORDER BY random() LIMIT 1', (w1, w2))
+            else:
+                c.execute('SELECT w3 FROM triples WHERE w1 == ? AND w2 == ? ORDER BY random() LIMIT 1', (w1, w2))
+            r = c.fetchone()
+            if r is None:
                 break
-            new = random.choice(cache[(w1, w2)])
+            else:
+                (new,) = r
             if new == '\n':
                 break
             w1, w2 = w2, new
--- tests/test_markovgen.py
+++ tests/test_markovgen.py
@@ -1,40 +1,40 @@
 from markovgen import Markov
-from unittest import TestCase
+from spanish_inquisition import Tribunal
 
-class MarkovgenTests(TestCase):
+class MarkovgenTests(Tribunal):
     def testForward(self):
-        self.assertRaises(ValueError, Markov().generate_markov_text)
+        self.expectRaises(ValueError, Markov().generate_markov_text)
         for x in range(1, 1000):
             l = Markov(['foo bar baz qux']).generate_markov_text()
-            self.assertIn(l, ['foo bar baz qux', 'bar baz qux', 'baz qux'])
+            self.expectIn(l, ['foo bar baz qux', 'bar baz qux', 'baz qux'])
 
     def testBackward(self):
-        self.assertRaises(ValueError, Markov().generate_markov_text)
+        self.expectRaises(ValueError, Markov().generate_markov_text)
         for x in range(1, 1000):
             l = Markov(['foo bar baz qux']).generate_markov_text(backward=True)
-            self.assertIn(l, ['foo bar baz qux', 'foo bar baz', 'foo bar'])
+            self.expectIn(l, ['foo bar baz qux', 'foo bar baz', 'foo bar'])
 
     def testForceSeedForward(self):
         for x in range(1, 1000):
             l = Markov(['foo bar baz qux']).generate_markov_text(
                     seed='bar')
-            self.assertIn(l, ['bar baz qux'])
+            self.expectIn(l, ['bar baz qux'])
         for x in range(1, 1000):
             l = Markov(['foo bar bar baz qux']).generate_markov_text(
                     seed=('bar', 'baz'))
-            self.assertIn(l, ['bar baz qux'])
+            self.expectIn(l, ['bar baz qux'])
 
     def testForceSeedBackward(self):
         for x in range(1, 1000):
             l = Markov(['foo bar baz qux']).generate_markov_text(
                     seed='bar', backward=True)
-            self.assertIn(l, ['foo bar'])
+            self.expectIn(l, ['foo bar'])
 
     def testAvailableSeeds(self):
         l = Markov(['foo bar baz qux']).available_seeds()
         l = {x for x in l if '\n' not in x}
-        self.assertEqual(l, {('foo', 'bar'), ('bar', 'baz'), ('baz', 'qux')})
+        self.expectEqual(l, {('foo', 'bar'), ('bar', 'baz'), ('baz', 'qux')})
         l = Markov(['foo bar baz qux']).available_seeds(backward=True)
         l = {x for x in l if '\n' not in x}
-        self.assertEqual(l, {('qux', 'baz'), ('baz', 'bar'), ('bar', 'foo')})
+        self.expectEqual(l, {('qux', 'baz'), ('baz', 'bar'), ('bar', 'foo')})
 
